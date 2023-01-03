9b96e93e3f35bafdcb1864371a9fcf6d5c69ed4a
Alex Kovrigin <a.kovrigin0@gmail.com>
2023-01-03 15:51:08 +0100

    Small code cleanup

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,41 +1,59 @@
 import numpy as np
-from tqdm import tqdm
-from typing import List, Tuple, Union
+from typing import List, Tuple, Union, Callable
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def get_data(experiment_name: str, count: int = 200, sample_size: int = 200, sep: str = ' ', n_plot: int = 5, plot_config: Union[List[Tuple[int, int]], None] = None):
+
+def get_data(experiment_name: str, count: int = 200, sample_size: int = 200, sep: str = ' ',
+             n_plotted_trajectories: int = 5,
+             plot_config: Union[List[Tuple[int, int]], None] = None) -> np.ndarray:
+    """
+    Gets trajectories data for the experiment
+    @param experiment_name:
+    @param count: Number of trajectories to read
+    @param sample_size: Number of points in each trajectory
+    @param sep: CSV separator
+    @param n_plotted_trajectories:
+    @param plot_config: Plots configurations: pairs of component indexes to plot
+    @return: Loaded trajectories as a numpy array
+    """
     ans = []
     for i in range(count):
         d = pd.read_csv(f"trajectories/{experiment_name}/{i}.csv", sep=sep, header=None)
         N = len(d)
         assert N >= sample_size
+        # take evenly spaced `sample_size` points
         ans.append(d.to_numpy()[::(N // sample_size)])
     X = np.array(ans)
-    # Plot
+
+    # Optionally, plot input trajectories
     if plot_config is not None:
-        X_plot = X[:n_plot]
+        # Plot only few trajectories
+        X_plot = X[:n_plotted_trajectories]
+        # plot for every configuration
         N = len(plot_config)
         fig, axs = plt.subplots(N)
         fig.suptitle(f"Input trajectories: {experiment_name}")
         for idx in range(N):
+            # components on the plot
             j, k = plot_config[idx]
             if N == 1:
                 x = axs
             else:
                 x = axs[idx]
             for traj in X_plot:
-                x.scatter(traj[:,j], traj[:,k])
+                x.scatter(traj[:, j], traj[:, k])
             x.set_xlabel(f"Component #{j}")
             x.set_ylabel(f"Component #{k}")
         fig.show()
     return X
 
-def sortby_H(X: np.ndarray, H) -> np.ndarray:
+def sortby_H(X: np.ndarray, H: Callable[[np.ndarray], float]) -> np.ndarray:
     """
-    Sorts data by Hamiltonian
-    
-    Makes it possible to plot beautiful distance matrices
+    Sorts data by Hamiltonian. Makes it possible to plot beautiful distance matrices.
+    @param X: Trajectories data
+    @param H: Hamiltonian
+    @return: Sorted trajectories data
     """
     Hs = np.array([H(x[0]) for x in X])
     return X[Hs.argsort()]
--- utils/manifold.py
+++ utils/manifold.py
@@ -1,14 +1,30 @@
+from typing import Tuple, List, Optional, Union, Any
+
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import ndarray
 from sklearn import neighbors
 import scipy
 
-def spectral_embedding(W: np.ndarray, n_neighbors: int = 20, alpha: float = 1.0, cutoff: float = 0.6, skip_plot=False):
-    N = W.shape[0]
-    nn_max = np.mean(np.sort(W)[:,n_neighbors+1])
-    eps = 2 * nn_max**2
-    # Calculate affinity matrix and get embedding
-    K = np.exp(-(W ** 2) / eps) # kernel matrix
+
+def spectral_embedding(W: np.ndarray, n_neighbors: int = 20, alpha: float = 1.0, cutoff: float = 0.6, do_plot=True,
+                       n_components: int = 20):
+    """
+    Calculates evals, embedding by distance matrix using diffusion maps
+    @param W: distance matrix
+    @param n_neighbors: number of neighbors for calculating epsilon
+    @param alpha:
+    @param cutoff: heuristic score threshold
+    @param do_plot: whether to plot components heuristic score
+    @param n_components: number of components for KNN in heuristic score
+    @return: evals, embedding and embed list
+    """
+    # Calculate epsilon
+    nn_max = np.mean(np.sort(W)[:, n_neighbors + 1])
+    eps = 2 * nn_max ** 2
+
+    # Kernel matrix
+    K = np.exp(-(W ** 2) / eps)
     alpha_norm = K.sum(axis=1) ** alpha
     K /= alpha_norm
     K /= alpha_norm[:, None]
@@ -22,36 +38,44 @@ def spectral_embedding(W: np.ndarray, n_neighbors: int = 20, alpha: float = 1.0,
 
     # Graph laplacian GL = 1 - L
     K *= -1
-    mean_shift = np.mean(diag / sqrt_norm**2)
+    mean_shift = np.mean(diag / sqrt_norm ** 2)
     np.fill_diagonal(K, 1 - mean_shift)
 
-    eigenvalues, eigenvectors = scipy.sparse.linalg.eigsh(-K, 20, which="LM", sigma=1.0, v0=None)
-    # print(eigenvalues, eigenvectors)
+    eigenvalues, eigenvectors = scipy.sparse.linalg.eigsh(-K, n_components, which="LM", sigma=1.0, v0=None)
 
     eigenvectors = eigenvectors[-2::-1]
-    eigenvalues = eigenvalues[-2::-1] # ignore eigenvalue = 1.0
+    eigenvalues = eigenvalues[-2::-1]
 
     # renormalize
     eigenvectors = (
-        np.sqrt(eigenvectors.shape[0]) * eigenvectors / np.linalg.norm(eigenvectors, axis=0)
+            np.sqrt(eigenvectors.shape[0]) * eigenvectors / np.linalg.norm(eigenvectors, axis=0)
     )
 
-    scores, embed_list = heuristic_score(eigenvalues, eigenvectors, cutoff, 5)
+    scores, embed_list = heuristic_score(eigenvalues, eigenvectors, cutoff)
 
     # Plot components
-    comp_color = [ "black" if x >= cutoff else "grey" for x in scores]
-    if not skip_plot:
+    comp_color = ["black" if x >= cutoff else "grey" for x in scores]
+    if do_plot:
         plt.bar(np.arange(1, len(scores) + 1), scores, color=comp_color)
-        plt.axhline(y=cutoff, color = 'blue', linestyle = '--')
+        plt.axhline(y=cutoff, color='blue', linestyle='--')
         plt.xlabel("Component")
         plt.ylabel("Score")
         plt.title(f"Components with cutoff {cutoff}, eps={eps}")
         plt.show()
     return scores, eigenvectors, embed_list
 
-def heuristic_score(evals, evecs, cutoff, n_neighbors):
+
+def heuristic_score(evals: np.ndarray, evecs: np.ndarray, cutoff: float, n_neighbors: int = 5):
+    """
+    Calculates heuristic score described in Appendix B of the paper
+    @param evals: eigenvalues of embedding
+    @param evecs: eigenvectors of embedding
+    @param cutoff: threshold of heuristic score
+    @param n_neighbors:
+    @return: scores and embed list
+    """
     n_components = evals.shape[0]
-    
+
     # length scale using log (see Appendix B)
     weights = np.empty_like(evals)
     weights[evals <= -1] = 0
