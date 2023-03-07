c05d82f68402b95427f52bb62603719fe1f05950
Alexander Kovrigin <a.kovrigin0@gmail.com>
2023-03-07 20:02:31 +0000

    generate low/high fix with scaler

--- utils/trajectory_generation.py
+++ utils/trajectory_generation.py
@@ -7,6 +7,10 @@ import creating.double_pendulum as dp
 import creating.coupled_oscillator as co
 
 
+def angle_norm(alpha):
+    return (alpha % (2 * np.pi)) - np.pi
+
+
 def my_save(data, experiment_name: str):
     N_traj = data.shape[0]
     for trajectory, i in zip(data, range(N_traj)):
@@ -21,7 +25,11 @@ def generate_all_trajectories(count=200, traj_len=1000):
     co.create_trajectories(count, traj_len=traj_len, normalize=False)
 
     # double pendulum low/high energies
-    dp_low_energies = dp.create_trajectories(count, traj_len, normalize=False, save=False, E_min=-3, E_max=-2)
-    dp_high_energies = dp.create_trajectories(count, traj_len, normalize=False, save=False, E_min=0, E_max=0)
-    my_save(dp_low_energies[0], "double_pendulum_low_energy")
-    my_save(dp_high_energies[0], "double_pendulum_high_energy")
+    dp_low_energies = dp.create_trajectories(count, traj_len, normalize=False, save=False, E_min=-3, E_max=-2)[0]
+    dp_high_energies = dp.create_trajectories(count, traj_len, normalize=False, save=False, E_min=0, E_max=0)[0]
+    # double pendulum angles to [-pi; pi]
+    dp_low_energies[:, :, :2] = np.vectorize(angle_norm)(dp_low_energies[:, :, :2])
+    dp_high_energies[:, :, :2] = np.vectorize(angle_norm)(dp_high_energies[:, :, :2])
+    # save
+    my_save(dp_low_energies, "double_pendulum_low_energy")
+    my_save(dp_high_energies, "double_pendulum_high_energy")
