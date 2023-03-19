33684945810f738ec9db241ba38ea12244684f59
antoniy.belishev <antoniy.belishev@gmail.com>
2023-03-19 21:26:09 +0100

    added normalization of the angle in double pendulum

--- creating/double_pendulum.py
+++ creating/double_pendulum.py
@@ -19,13 +19,40 @@ energy1 = lambda state: energy12(state, 1)
 energy2 = lambda state: energy12(state, -1)
 
 
-def create_trajectories(N_traj=1000, traj_len=500, normalize=True, save=True):
+def normalize_angle(angle):
+    while angle > np.pi:
+        angle -= 2 * np.pi
+    while angle < -np.pi:
+        angle += np.pi
+    return angle
+
+def normalize_state(state):
+    return np.array([normalize_angle(state[0]), normalize_angle(state[1]), *state[2:]])
+
+
+normalize_state = np.vectorize(normalize_state, signature="(m)->(m)")
+
+
+def create_trajectories(N_traj=1000,
+                        traj_len=500,
+                        normalize=True,
+                        save=True,
+                        E_min=-3,
+                        E_max=0,
+                        min_deviation_threshold=1e-04,
+                        max_deviation_threshold=1e-02,
+                        T=40000):
     """
     Creates trajectories of double pendulum with different energies.
     Returns trajectories, energies and energies of two modes
     @param N_traj: number of created trajectories
     @param normalize: whether to normalize trajectories in a way that maximum absolute value along each coordinate is 1 or not
     @param save: whether to save trajectories and energies to trajectories/double_pendulum or not
+    @param E_min: lower bound for energy
+    @param E_max: upper bound for energy
+    @param min_deviation_threshold: parameter controlling the precision
+    @param max_deviation_threshold: parameter controlling the precision
+    @param T: the time during which to generate trajectory
     @return data: 3d array containing all created trajectories
     @return energies: energies of each trajectory
     @return energies1: energies of the first mode for each trajectory
@@ -48,7 +75,7 @@ def create_trajectories(N_traj=1000, traj_len=500, normalize=True, save=True):
         return np.array([theta1dot, theta2dot, ptheta1dot, ptheta2dot])
 
     def state0_generator():
-        total_energy = np.random.uniform(-3, 0)
+        total_energy = np.random.uniform(E_min, E_max)
         kinetic_energy = -1
         while kinetic_energy < 0:
             theta1, theta2 = np.random.uniform(-np.pi / 4, np.pi / 4, size=2)
@@ -58,7 +85,13 @@ def create_trajectories(N_traj=1000, traj_len=500, normalize=True, save=True):
         p1 = p2 * np.cos(theta1 - theta2) + np.sqrt((p2 * np.cos(theta1 - theta2)) ** 2 + kinetic_energy - 2 * p2 ** 2)
         return [theta1, theta2, p1, p2]
     
-    data = np.array([generate_traj(derivative, state0_generator, energy, "absolute", 0.1, 100, max_deviation_threshold=0.1, traj_len=traj_len) for _ in tqdm(range(N_traj))])
+    # data = np.array([generate_traj(derivative, state0_generator, energy, "absolute", 0.01, T,
+    #                                min_deviation_threshold=min_deviation_threshold,
+    #                                max_deviation_threshold=max_deviation_threshold,
+    #                                traj_len=traj_len)
+    #                                for _ in tqdm(range(N_traj))])
+    data = np.array([generate_traj(derivative, state0_generator, energy, "absolute", 0.1, T, traj_len=traj_len, min_deviation_threshold=min_deviation_threshold, max_deviation_threshold=max_deviation_threshold) for _ in tqdm(range(N_traj))])
+    data = normalize_state(data)
     energies = np.array([energy(traj[0]) for traj in data])
     energies1 = np.array([energy1(traj[0]) for traj in data])
     energies2 = np.array([energy2(traj[0]) for traj in data])
@@ -73,5 +106,5 @@ def create_trajectories(N_traj=1000, traj_len=500, normalize=True, save=True):
         np.savetxt("trajectories/double_pendulum/energies.csv", energies)
         np.savetxt("trajectories/double_pendulum/energies1.csv", energies1)
         np.savetxt("trajectories/double_pendulum/energies2.csv", energies2)
-
+    
     return data, energies, energies1, energies2
