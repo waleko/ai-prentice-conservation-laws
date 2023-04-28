47391c1ad3e204109c9f0d519e52e9e91cd34830
Alexander Kovrigin <a.kovrigin0@gmail.com>
2023-04-28 19:25:48 +0000

    remove traj gen from utils package

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,6 @@
 from .data_loader import *
 from .dist_matrix import *
 from .manifold import *
-from .trajectory_generation import *
 from .experiment_data import *
 from .scores import *
 from .metrics import *
