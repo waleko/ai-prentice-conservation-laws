3dc4a77ff049a8e532f2c7a6a2a4a79eb01ada7c
antoniy.belishev <antoniy.belishev@gmail.com>
2023-05-01 10:51:34 +0200

    slight changes

--- utils/scores.py
+++ utils/scores.py
@@ -20,7 +20,7 @@ class OrderScore:
         return (np.abs(k_nearest_orders - self.orders) ** p).mean() ** (1 / p) / len(embed)
 
 
-def order_scores(dist_matrix, UMAP_kwargs={"n_epochs": 2000, "n_neighbors": 80}, n_dims_arr=np.arange(1, 7), n_reference=20, k_nearest=40, check_periodic_1d=True):
+def order_scores(dist_matrix, UMAP_kwargs={"n_neighbors": 80}, n_dims_arr=np.arange(1, 7), n_reference=20, k_nearest=40, check_periodic_1d=True):
     Score = OrderScore(dist_matrix, n_reference, k_nearest)
     scores_arr = []
     if check_periodic_1d:
@@ -29,13 +29,4 @@ def order_scores(dist_matrix, UMAP_kwargs={"n_epochs": 2000, "n_neighbors": 80},
     for n_dims in tqdm(n_dims_arr):
         embed = UMAP(metric="precomputed", n_components=n_dims, **UMAP_kwargs).fit_transform(dist_matrix)
         scores_arr.append(Score.order_score(embed))
-    return scores_arr
-
-
-def order_scores_different_n_ref(dist_matrix, n_reference_arr, n_repeats=5000, UMAP_kwargs={"n_epochs": 2000, "n_neighbors": 80}, n_dims_arr=np.arange(1, 7), k_nearest=40):
-    Score_arr = [[OrderScore(dist_matrix, n_reference, k_nearest) for _ in range(n_repeats)] for n_reference in n_reference_arr]
-    scores_arr = []
-    for n_dims in tqdm(n_dims_arr):
-        embed = UMAP(metric="precomputed", n_components=n_dims, **UMAP_kwargs).fit_transform(dist_matrix)
-        scores_arr.append([[Score.order_score(embed) for Score in row] for row in Score_arr])
     return scores_arr
